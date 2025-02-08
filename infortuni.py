bf84cf77b2ec440a41ab9249983b6a11a525f21a
Fabio Lolicato <lolicato.fabio@gmail.com>
2025-02-08 14:48:52 +0100

    Update infortuni.py

--- infortuni.py
+++ infortuni.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import random
 import os
+import chardet
 
 def extract_team_and_player(filename):
     """Extract team and player names from the filename."""
@@ -11,6 +12,12 @@ def extract_team_and_player(filename):
     team = parts[0]
     player = os.path.splitext(parts[1])[0]  # Remove file extension
     return team, player
+
+def detect_encoding(filepath):
+    """Detects the encoding of a file."""
+    with open(filepath, 'rb') as f:
+        result = chardet.detect(f.read())
+    return result['encoding']
 
 def process_files(directory):
     """Processes files from a specified server directory and organizes data by team."""
@@ -24,10 +31,12 @@ def process_files(directory):
                 if team not in team_players:
                     team_players[team] = []
                 
-                # Read file and extract second column, ignoring the first row
-                df = pd.read_csv(filepath, header=None, skiprows=1)
+                # Detect encoding and read file
+                encoding = detect_encoding(filepath)
+                df = pd.read_csv(filepath, header=None, skiprows=1, encoding=encoding, dtype=str)
+                
                 if df.shape[1] > 1:
-                    team_players[team].extend(df.iloc[:, 1].dropna().tolist())
+                    team_players[team].extend(df.iloc[:, 1].dropna().str.encode('latin1').decode('utf-8', 'ignore').tolist())
     
     return team_players
 
@@ -58,3 +67,4 @@ if target_button:
     st.write("## Selected Players")
     result_df = pd.DataFrame([(team, ", ".join(players)) for team, players in selected_players.items()], columns=["Team", "Selected Players"])
     st.dataframe(result_df)
+
