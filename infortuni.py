52a2894f10ac8d63f4e4f0795cca5df4fd217674
Fabio Lolicato <lolicato.fabio@gmail.com>
2025-02-08 14:53:00 +0100

    Update infortuni.py

--- infortuni.py
+++ infortuni.py
@@ -31,8 +31,7 @@ def process_files(directory):
                     df = pd.read_csv(filepath, header=None, skiprows=1, encoding='latin1', dtype=str)
                 
                 if df.shape[1] > 1:
-                    players = df.iloc[:, 1].dropna().tolist()
-                    players = [p.encode('latin1').decode('utf-8') for p in players]  # Fix encoding issues
+                    players = df.iloc[:, 1].dropna().astype(str).tolist()
                     team_players[team].extend(players)
     
     return team_players
@@ -64,4 +63,3 @@ if target_button:
     st.write("## Selected Players")
     result_df = pd.DataFrame([(team, ", ".join(players)) for team, players in selected_players.items()], columns=["Team", "Selected Players"])
     st.dataframe(result_df)
-
