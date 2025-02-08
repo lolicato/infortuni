6a6b16de4709efb1617b0b37ce742e8e151f544c
Fabio Lolicato <lolicato.fabio@gmail.com>
2025-02-08 14:54:45 +0100

    Update infortuni.py

--- infortuni.py
+++ infortuni.py
@@ -25,13 +25,15 @@ def process_files(directory):
                     team_players[team] = []
                 
                 # Read file and extract second column, ignoring the first row
-                try:
-                    df = pd.read_csv(filepath, header=None, skiprows=1, encoding='utf-8', dtype=str)
-                except UnicodeDecodeError:
-                    df = pd.read_csv(filepath, header=None, skiprows=1, encoding='ISO-8859-1', dtype=str)
+                for encoding in ['utf-8', 'ISO-8859-1', 'latin1']:
+                    try:
+                        df = pd.read_csv(filepath, header=None, skiprows=1, encoding=encoding, dtype=str)
+                        break
+                    except UnicodeDecodeError:
+                        continue
                 
                 if df.shape[1] > 1:
-                    players = df.iloc[:, 1].dropna().astype(str).apply(lambda x: x.encode('ISO-8859-1').decode('utf-8')).tolist()
+                    players = df.iloc[:, 1].dropna().astype(str).tolist()
                     team_players[team].extend(players)
     
     return team_players
@@ -63,4 +65,3 @@ if target_button:
     st.write("## Selected Players")
     result_df = pd.DataFrame([(team, ", ".join(players)) for team, players in selected_players.items()], columns=["Team", "Selected Players"])
     st.dataframe(result_df)
-
